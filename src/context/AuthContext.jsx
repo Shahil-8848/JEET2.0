7512c2e464c0261e20bf19382d85d3b4bf44b89a
Shahil Chettri <shahil.budathoki@crystalsolution.com.np>
2025-12-11 22:03:18 +0545

    Redirect Path resolved

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -86,6 +86,7 @@ export const AuthProvider = ({ children }) => {
                     data: {
                         full_name: fullName,
                     },
+                    emailRedirectTo: window.location.origin,
                 },
             });
             if (error) throw error;
